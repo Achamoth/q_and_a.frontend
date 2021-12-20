eca4fd3d226deef5768f19434d75fe273c43b90b
Ammar Abu Shamleh <ammar.abushamleh@health.telstra.com>
2021-12-20 22:47:18 +0800

    Style pseudo-classes and nested elements

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,6 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 import { fontFamily, fontSize, gray1, gray2, gray5 } from "./Styles";
+import { UserIcon } from "./Icons";
 
 export const Header = () => {
   const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -23,13 +24,56 @@ export const Header = () => {
         box-shadow: 0 3px 7px 0 rgba(110, 112, 114, 0.21);
       `}
     >
-      <a href="./">Q & A</a>
+      <a
+        href="./"
+        css={css`
+          font-size: 24px;
+          font-weight: bold;
+          color: ${gray1};
+          text-decoration: none;
+        `}
+      >
+        Q & A
+      </a>
       <input
         type="text"
         placeholder="Search..."
         onChange={handleSearchInputChange}
+        css={css`
+          box-sizing: border-box;
+          font-family: ${fontFamily};
+          font-size: ${fontSize};
+          padding: 8px 10px;
+          border: 1px solid ${gray5};
+          border-radius: 3px;
+          color: ${gray2};
+          background-color: white;
+          width: 200px;
+          height: 30px;
+          :focus {
+            outline-color: ${gray5};
+          }
+        `}
       />
-      <a href="./signin">
+      <a
+        href="./signin"
+        css={css`
+          font-family: ${fontFamily};
+          font-size: ${fontSize};
+          padding: 5px 10px;
+          background-color: transparent;
+          color: ${gray2};
+          text-decoration: none;
+          cursor: pointer;
+          :focus {
+            outline-color: ${gray5};
+          }
+          span {
+            margin-left: 7px; // Equivalent to applying directly to span element below
+          }
+        `}
+      >
+        <UserIcon />
         <span>Sign In</span>
       </a>
     </div>
